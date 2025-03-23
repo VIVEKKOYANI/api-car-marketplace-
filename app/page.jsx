918a80eb6ef498b0d1b966a3d531ade484c2100e
vivek Koyani <vivek.koyani@gmail.com>
2025-03-24 02:47:37 +0530

    add a section for Featured Cars in main page

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,9 @@
+import { ChevronRight } from "lucide-react";
+import CarCard from "@/components/car-card";
 import HoemSearch from "@/components/home-search";
+import { Button } from "@/components/ui/button";
+import { featuredCars } from "@/lib/data";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -6,7 +11,7 @@ export default function Home() {
       {/* Hero */}
 
       <section className="relative py-16 md:py-28 dotted-background">
-        <div className="max-w-4xl mx-auto text-center"> 
+        <div className="max-w-4xl mx-auto text-center">
           <div className="mb-8">
             <h1 className="text-5xl md:text-8xl mb-4 gradient-title">Find your Dream Car with Vehiql AI</h1>
             <p className="text-xl text-gray-500 mb-8 max-w-2xl mx-auto">
@@ -16,6 +21,25 @@ export default function Home() {
 
           {/* Search */}
           <HoemSearch />
+        </div>
+      </section>
+
+      <section className="py-12">
+        <div className="container mx-auto px-4">
+          <div className="flex justify-between items-center mb-8">
+            <h2 className="text-2xl font-bold">Featured Cars</h2>
+            <Button variant="ghost" className="flex items-center" asChild>
+              <Link href="/cars">
+                View All <ChevronRight className="ml-1 h-4 w-4" />
+              </Link>
+            </Button>
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {featuredCars.map((car) => 
+              <CarCard key={car.id} car={car} />
+            )}
+          </div>
         </div>
       </section>
     </div>
