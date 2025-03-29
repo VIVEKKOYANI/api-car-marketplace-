2a0573aa17b0b96f6fcbc81e3704f17f7bd20f6d
vivek Koyani <vivek.koyani@gmail.com>
2025-03-29 15:49:48 +0530

    add new pages

---
+++ app/(admin)/admin/layout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AdminLayout() {
+  return (
+    <div>layout</div>
+  )
+}
+
+export default AdminLayout
---
+++ app/(main)/cars/[id]/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const CarPage = async ({ params }) => {
+  const { id } = params;
+  return (
+    <div>page</div>
+  )
+}
+
+export default CarPage;
