6e9f77689333e9f49740adc83ffe2647207b28a1
vivek Koyani <vivek.koyani@gmail.com>
2025-04-24 22:38:21 +0530

    processCarImageWithAI api add and create some component

---
+++ app/(admin)/admin/cars/_components/add-car-form.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddCarForm() {
+  return (
+    <div>AddCarForm</div>
+  )
+}
+
+export default AddCarForm
--- app/(admin)/admin/cars/create/page.jsx
+++ app/(admin)/admin/cars/create/page.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import AddCarForm from '../_components/add-car-form'
 
 export const metadata = {
   title: "Add New Car | Vehiql Admin",
@@ -9,6 +10,7 @@ function AddCarPage() {
   return (
     <div className='p-6'>
       <h1 className='text-2xl font-bold mb-6'>Add New Car</h1>
+      <AddCarForm />
     </div>
   )
 }
